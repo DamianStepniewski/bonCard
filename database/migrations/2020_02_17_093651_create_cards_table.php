31b7bdbb14656be46bd55cc7d3f45b26d902504b
Snikk <snikk@snikk.pl>
2020-02-17 11:18:21 +0100

    Removed unnecessary controllers, added cards table migration, model and controller.

---
+++ database/migrations/2020_02_17_093651_create_cards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->unique();
+            $table->smallInteger('pin');
+            $table->date('activation_date');
+            $table->date('expiration_date');
+            $table->float('balance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
